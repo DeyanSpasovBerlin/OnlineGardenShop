e7522f7886309b25a41ec7ce1ab321714ffa00d4
DeyanSpasovBerlin <pislpe@gmail.com>
2025-03-02 18:31:04 +0100

    Layer

---
+++ src/main/java/finalproject/onlinegardenshop/exeption/OnlineGardenSchopBadRequestException.java
@@ -0,0 +1,9 @@
+package finalproject.onlinegardenshop.exeption;
+
+public class OnlineGardenSchopBadRequestException  extends RuntimeException{
+
+    public OnlineGardenSchopBadRequestException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/finalproject/onlinegardenshop/exeption/OnlineGardenShopResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package finalproject.onlinegardenshop.exeption;
+
+public class OnlineGardenShopResourceNotFoundException extends RuntimeException{
+    public OnlineGardenShopResourceNotFoundException(String message) {
+        super(message);
+    }
+}
