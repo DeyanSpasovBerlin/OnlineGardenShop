23557553cd906552bd63d74d35add4908a49e2d8
DeyanSpasovBerlin <pislpe@gmail.com>
2025-03-15 11:58:34 +0100

    FIN-32

---
+++ src/main/java/finalproject/onlinegardenshop/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package finalproject.onlinegardenshop.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/finalproject/onlinegardenshop/scheduler/ScheduledTask.java
@@ -0,0 +1,11 @@
+package finalproject.onlinegardenshop.scheduler;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class ScheduledTask {
+
+
+}
