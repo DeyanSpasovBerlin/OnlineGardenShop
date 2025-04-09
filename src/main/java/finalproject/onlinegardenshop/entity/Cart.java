d5ec25b18f711c5da8fed517dd77e6e30c412867
DeyanSpasovBerlin <pislpe@gmail.com>
2025-04-09 19:11:11 +0200

    FIN-57-V3

--- src/main/java/finalproject/onlinegardenshop/entity/Cart.java
+++ src/main/java/finalproject/onlinegardenshop/entity/Cart.java
@@ -23,26 +23,19 @@ public class Cart {
     private Integer id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "users_id", columnDefinition = "int", nullable = true)//-> , nullable = true- alow delete.user
+    @JoinColumn(name = "users_id", columnDefinition = "int", nullable = true)
     private Users users;
 
-    @OneToMany(mappedBy = "cart", cascade = {CascadeType.PERSIST, CascadeType.MERGE}) // Add CascadeType.ALL here
+    @OneToMany(mappedBy = "cart", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<CartItems> cartItems;
 
-//    @Column(name = "completed", nullable = false)// add to cart table!ето для более научное отношение к Cart
-    //мы не только del CartItems, но и ставим falg completed на Cart, что бы указать, что ее содержание через
-    //checkout(Integer userId) записано в Orders. Более простой вариант которой я сделал CartItems del-> Cart empty
-//    private boolean completed = false;
-
-    //Ето нам нужно что бы можно в Scheduled отсчитать 10 минут от посленего добавления CartItems
     @Column(name = "last_updated", nullable = false)
     private LocalDateTime lastUpdated;
 
-    // Automatically update 'lastUpdated' before persisting or updating
-    @PreUpdate// Runs before the entity is updated (modified in the database).
-    @PrePersist// Runs before the entity is first saved (inserted into the database).
+    @PreUpdate
+    @PrePersist
     protected void onUpdate() {
-        this.lastUpdated = LocalDateTime.now();//automatically set to LocalDateTime.now().
+        this.lastUpdated = LocalDateTime.now();
     }
 
 
