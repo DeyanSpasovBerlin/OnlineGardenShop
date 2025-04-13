85542a593147c7d807c119d7cfa51deb3730f3b9
DeyanSpasovBerlin <pislpe@gmail.com>
2025-04-13 16:23:27 +0200

    FIN-61

--- src/test/java/finalproject/onlinegardenshop/OnlineGardenShopApplicationTests.java
+++ src/test/java/finalproject/onlinegardenshop/OnlineGardenShopApplicationTests.java
@@ -3,7 +3,7 @@ package finalproject.onlinegardenshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=dev")
 class OnlineGardenShopApplicationTests {
 
     @Test
